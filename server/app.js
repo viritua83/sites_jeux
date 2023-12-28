8c600c69f6888079181e53fff133d3b0de119376
omni <omni.omniscience@gmail.com>
2023-12-28 01:15:05 +0100

    chat utilisateur

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Stockage des utilisateurs connectés
+const connectedUsers = {};
+
 // Serveur HTTP
 const port = 3000;
 server.listen(port, () => {
@@ -16,16 +19,23 @@ server.listen(port, () => {
 io.on('connection', (socket) => {
   console.log('Un utilisateur s\'est connecté');
 
+  // Gestion de la connexion de l'utilisateur
+  socket.on('user connected', (username) => {
+    connectedUsers[socket.id] = username;
+    io.emit('chat message', `[${username} a rejoint le chat]`);
+  });
+
   // Gestion des messages du client vers le serveur
   socket.on('chat message', (msg) => {
-    console.log(`Message reçu : ${msg}`);
-    
-    // Diffuser le message à tous les clients connectés
-    io.emit('chat message', msg);
+    const username = connectedUsers[socket.id];
+    io.emit('chat message', `[${username}]: ${msg}`);
   });
 
   // Gestion de la déconnexion de l'utilisateur
   socket.on('disconnect', () => {
+    const username = connectedUsers[socket.id];
+    delete connectedUsers[socket.id];
+    io.emit('chat message', `[${username} a quitté le chat]`);
     console.log('Un utilisateur s\'est déconnecté');
   });
 });
